6c71645e8c4dde160f38fed172d3f48eecce4889
XJ Hsu <xj11400@gmail.com>
2025-12-27 02:50:10 +0800

    feat(templates): add comapt and flake for go

---
+++ templates/flake/go/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A simple hello world Go application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    flake-lib = {
+      url = "github:xj11400/flake-lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-lib,
+      ...
+    }@inputs:
+    let
+    in
+    flake-lib.lib.system.forEachSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "hello-go";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = null;
+          };
+        };
+
+        devShells =
+          let
+          in
+          {
+            default = pkgs.mkShell {
+              nativeBuildInputs = [
+                pkgs.go
+                pkgs.gopls
+              ];
+            };
+          };
+      }
+    );
+}
