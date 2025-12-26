aba45ca3b7871d2092fad071c49a0cdfe7869e9b
XJ Hsu <xj11400@gmail.com>
2025-12-27 02:50:05 +0800

    feat(templates): add comapt and flake for cpp

---
+++ templates/compat/cpp/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A simple hello world C++ application with CMake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    flake-lib = {
+      url = "github:xj11400/flake-lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-lib,
+      ...
+    }@inputs:
+    let
+    in
+    flake-lib.lib.system.forEachSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = pkgs.callPackage ./default.nix { };
+        };
+
+        devShells = {
+          default = pkgs.callPackage ./shell.nix { };
+        };
+      }
+    );
+}
