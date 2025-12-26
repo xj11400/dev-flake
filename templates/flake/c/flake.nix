6c05722f339710fefbb40db2a5e8f01c29b321a5
XJ Hsu <xj11400@gmail.com>
2025-12-27 02:50:00 +0800

    feat(templates): add comapt and flake for c

---
+++ templates/flake/c/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "A simple hello world C application with Makefile";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    flake-lib = {
+      url = "github:xj11400/flake-lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-lib,
+      ...
+    }@inputs:
+    let
+    in
+    flake-lib.lib.system.forEachSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "hello-c";
+            version = "0.1.0";
+            src = ./.;
+            nativeBuildInputs = [ pkgs.gnumake ];
+            buildPhase = "make";
+            installPhase = "make install";
+          };
+        };
+
+        devShells =
+          let
+          in
+          {
+            default = pkgs.mkShell {
+              nativeBuildInputs = with pkgs; [
+                gcc
+                gnumake
+              ];
+            };
+          };
+      }
+    );
+}
